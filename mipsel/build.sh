45084ee582199e033e98e8ae3efc4f764c21b1da
Balazs Kilvady <kilvadyb@homejinni.com>
2012-10-17 08:47:56 +0000

    Force pkg-config and dbus include added; path fix.

--- mipsel/build.sh
+++ mipsel/build.sh
@@ -4,7 +4,7 @@ compile() {
     if [ -z $skip_git ]; then
         # Mips patch for qt3d
         cd qt3d
-        patch -p 1 < ../../qt5-tools/cross-tools/qt3d_assimp_mips_fix.diff || exit 1
+        patch -p 1 < ../../qt5-tools/mipsel/qt3d_assimp_mips_fix.diff || exit 1
         cd ..
     fi
 
@@ -13,8 +13,9 @@ compile() {
     cp ../qt5-tools/$ARCH/qmake.conf qtbase/mkspecs/linux-mipsel-g++/
     #git apply ../qt5-tools/cross-tools/qtjsbackend.patch --directory=qtjsbackend
 
+    export PKG_CONFIG_LIBDIR=/usr/mipsel-linux-gnu/lib/pkgconfig
     if [ -z $skip_qtbase ]; then
-        ./configure -arch mipsel -xplatform linux-mipsel-g++ -opensource -confirm-license -no-pch -nomake examples -nomake demos -nomake tests -no-gtkstyle -nomake translations -qt-zlib -qt-libpng -qt-libjpeg -qt-sql-sqlite -release -prefix $QTDIR -v
+        ./configure -arch mipsel -xplatform linux-mipsel-g++ -opensource -confirm-license -no-pch -nomake examples -nomake demos -nomake tests -no-gtkstyle -nomake translations -qt-zlib -qt-libpng -qt-libjpeg -qt-sql-sqlite -release -prefix $QTDIR -v -I /usr/mipsel-linux-gnu/include/dbus-1.0 -force-pkg-config
 
         cd qtbase && make $THREADS && make install && cd ..
         if [ $? -ne 0 ] ; then
